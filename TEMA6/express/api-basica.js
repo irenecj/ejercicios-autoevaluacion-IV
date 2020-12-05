cda41a410e00e4f82f5155060f94b3087181dcc6
Irene Cano Jerez <irenecanjer@correo.ugr.es>
2020-12-05 14:27:35 +0100

    Añado api básica

---
+++ TEMA6/express/api-basica.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const port = process.env.port || 8080
+
+var usuario = 'Irene'
+
+app.get('/', (req, res) => {
+  res.send({ message: '¡La página principal se muestra correctamente!.'})
+})
+
+app.get('/bienvenida', (req, res) => {
+  res.send({ message: 'Bienvenid@ ' + usuario})
+})
+
+app.get('/asignatura/:asig', (req, res) => {
+  var asignatura = req.params.asig
+  res.send({ message: 'Esta es la sección de información sobre la asignatura ' + asignatura})
+})
+
+app.listen(port, () => {
+  console.log('El servidor se encuentra escuchando en el puerto ' + port);
+})
+
+module.exports = app
--- etcd/miniprograma.js
+++
@@ -1,9 +0,0 @@
-const { Etcd3 } = require('etcd3');
-const cliente = new Etcd3();
-
-(async () => {
-  const clave = await cliente.get('claveIrene').string();
-  console.log("La clave de Irene es: ", clave);
-
-  await cliente.delete().all()
-})();
